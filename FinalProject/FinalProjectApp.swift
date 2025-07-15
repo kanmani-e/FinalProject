d71380a3c133ba538055c06470c7708c0f8f603f
Kanmani Elangovan <kanmani.elan@gmail.com>
2025-07-15 11:16:45 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Kanmani Elangovan on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
