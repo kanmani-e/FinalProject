5c7599b06b1107bd03634528a2f6c426e32be09a
Emily Suriel <emilysuriel0@gmail.com>
2025-07-16 13:35:59 -0400

    Created the layout of the About us page, by using placeholders for images and text.

--- FinalProject/AboutUsPage.swift
+++ FinalProject/AboutUsPage.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct AboutUsPage: View {
     var body: some View {
         ZStack{
+            Color(red: 0.97, green: 0.96, blue: 0.922)
+                .ignoresSafeArea()
             VStack{
                 Text("About Us")
                     .font(.title)
@@ -18,8 +20,53 @@ struct AboutUsPage: View {
                     Spacer()
                 
                 HStack{
+                    VStack{
+                        Image("placeholder")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(100)
+                        Text("Name")
+                        Text("Put Information Here")
+                    }//closing of VStack
+                    .padding(.horizontal, 5)
+                    
+                    VStack{
+                        Image("placeholder")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(100)
+                        Text("Name")
+                        Text("Put Information Here")
+                    }//Closing of VStack
+                    .padding(.horizontal, 5)
                     
                 }//closing of HStack
+                .padding(.horizontal, 30)
+                Spacer()
+                
+                HStack{
+                    VStack{
+                        Image("placeholder")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(100)
+                        Text("Name")
+                        Text("Put Information Here")
+                    }//closing of VStack
+                    .padding(.horizontal, 5)
+                    
+                    VStack{
+                        Image("placeholder")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(100)
+                        Text("Name")
+                        Text("Put Information Here")
+                    }//Closing of VStack
+                    .padding(.horizontal, 5)
+                }//Closing of HStack
+                .padding(.horizontal, 30)
+                Spacer()
             }//closing of VStack
         }//closing of ZStack
     }
