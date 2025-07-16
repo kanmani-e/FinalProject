8434f3584f65779f7624d580399b881d50522cd7
Emily Suriel <emilysuriel0@gmail.com>
2025-07-16 11:45:04 -0400

    Changed the look of the Navigation page and connected the UserInfo page to the NavigationPage.

--- FinalProject/NavigationPage.swift
+++ FinalProject/NavigationPage.swift
@@ -10,44 +10,60 @@ import SwiftUI
 struct NavigationPage: View {
     var body: some View {
         NavigationStack{
-            VStack{
-                Text("Placeholder")
-                    .font(.custom("Lobster-Regular", size: 48))
-                    .fontWeight(.bold)
-                    .padding(.top, 50)
-                Spacer()
+            ZStack{
                 
-                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                    Text("Pop ups")
-                        .font(.custom("Lobster-Regular", size: 30))
-                        .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
-                        .background(Color(red: 0.97, green: 0.96, blue: 0.922))
-                        .cornerRadius(10)
-                        .padding(.bottom, 100)
-                }//closing of NavLink
+                Color(red: 0.835, green: 0.333, blue: 0.314) // backgroud color
+                    .ignoresSafeArea()
                 
-                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                    Text("Resturants")
-                        .font(.custom("Lobster-Regular", size: 30))
-                        .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
-                        .background(Color(red: 0.97, green: 0.96, blue: 0.922))
-                        .cornerRadius(10)
-                        .padding(.bottom, 100)
-                }//closing of NavLink
-                
-                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                    Text("About Us")
-                        .font(.custom("Lobster-Regular", size: 30))
-                        .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
-                        .background(Color(red: 0.97, green: 0.96, blue: 0.922))
-                        .cornerRadius(10)
-                        .padding(.bottom, 200)
-                }//closing of NavLink
-            }//closing of VStack
+                VStack{
+                    
+                    Text("Placeholder")
+                        .font(.custom("Lobster-Regular", size: 48))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.159, green: 0.225, blue: 0.372))
+                        .padding(.top, 50)
+                    Spacer()
+                    
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Pop ups")
+                            .font(.custom("Lobster-Regular", size: 30))
+                            .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
+                            .padding(.horizontal, 24)
+                            .padding(.vertical, 12)
+                            .background(Color(red: 0.97, green: 0.96, blue: 0.922))
+                            .cornerRadius(10)
+                            .padding(.bottom, 100)
+                    }//closing of NavLink
+                    
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Resturants")
+                            .font(.custom("Lobster-Regular", size: 30))
+                            .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
+                            .padding(.horizontal, 24)
+                            .padding(.vertical, 12)
+                            .background(Color(red: 0.97, green: 0.96, blue: 0.922))
+                            .cornerRadius(10)
+                            .padding(.bottom, 100)
+                    }//closing of NavLink
+                    
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("About Us")
+                            .font(.custom("Lobster-Regular", size: 30))
+                            .foregroundColor(Color(red: 0.835, green: 0.333, blue: 0.314))
+                            .padding(.horizontal, 24)
+                            .padding(.vertical, 12)
+                            .background(Color(red: 0.97, green: 0.96, blue: 0.922))
+                            .cornerRadius(10)
+                            .padding(.bottom, 200)
+                    }//closing of NavLink
+                }//closing of VStack
+            }//closing of ZStack
         }//closing of NavStack
     }
 }
 
 #Preview {
-    NavigationPage()
+    NavigationStack{
+        NavigationPage()
+    }
 }
--- FinalProject/UserInfo.swift
+++ FinalProject/UserInfo.swift
@@ -14,83 +14,86 @@ struct UserInfo: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        VStack(spacing: 40) {
-            Text("Budget Slider") //title
-                .font(.title)
-                .bold()
-
-            VStack(spacing: 16) {
-                Text("Utilize the slider to select your budget!")
-                    .font(.headline)
-
-                GeometryReader { geometry in
-                    let sliderWidth = geometry.size.width - 32 //slider to for the hotdog
-
-                    ZStack(alignment: .leading) {
-                        Capsule()
-                            .fill(Color.gray.opacity(0.3))
-                            .frame(height: 8) //modifications
-
-                        Capsule()
-                            .fill(Color.gray)
-                            .frame(width: sliderWidth * CGFloat((budget - 5) / 95), height: 8)
-
-                        Image("hotdog") //hotdog slider
-                            .resizable()
-                            .frame(width: 60, height: 30)
-                            .scaleEffect(hotdogBounce ? 1.1 : 1.0)
-                            .offset(x: sliderWidth * CGFloat((budget - 5) / 95))
-                            .gesture(
-                                DragGesture()
-                                    .onChanged { value in
-                                        let location = max(0, min(Double(value.location.x), Double(sliderWidth)))
-                                        let percent = location / Double(sliderWidth)
-                                        budget = 5 + percent * 95
-                                    }
-                                    .onEnded { _ in
-                                        withAnimation(.interpolatingSpring(stiffness: 300, damping: 5)) {
-                                            hotdogBounce = true
+        NavigationStack{
+            VStack(spacing: 40) {
+                Text("Budget Slider") //title
+                    .font(.title)
+                    .bold()
+                
+                VStack(spacing: 16) {
+                    Text("Utilize the slider to select your budget!")
+                        .font(.headline)
+                    
+                    GeometryReader { geometry in
+                        let sliderWidth = geometry.size.width - 32 //slider to for the hotdog
+                        
+                        ZStack(alignment: .leading) {
+                            Capsule()
+                                .fill(Color.gray.opacity(0.3))
+                                .frame(height: 8) //modifications
+                            
+                            Capsule()
+                                .fill(Color.gray)
+                                .frame(width: sliderWidth * CGFloat((budget - 5) / 95), height: 8)
+                            
+                            Image("hotdog") //hotdog slider
+                                .resizable()
+                                .frame(width: 60, height: 30)
+                                .scaleEffect(hotdogBounce ? 1.1 : 1.0)
+                                .offset(x: sliderWidth * CGFloat((budget - 5) / 95))
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { value in
+                                            let location = max(0, min(Double(value.location.x), Double(sliderWidth)))
+                                            let percent = location / Double(sliderWidth)
+                                            budget = 5 + percent * 95
                                         }
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                            withAnimation {
-                                                hotdogBounce = false
+                                        .onEnded { _ in
+                                            withAnimation(.interpolatingSpring(stiffness: 300, damping: 5)) {
+                                                hotdogBounce = true
+                                            }
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                                withAnimation {
+                                                    hotdogBounce = false
+                                                }
                                             }
                                         }
-                                    }
-                            )
+                                )
+                        }
+                        .padding(.horizontal, 16)
                     }
-                    .padding(.horizontal, 16)
+                    .frame(height: 40)
+                    
+                    HStack {
+                        Text("$5")
+                        Spacer()
+                        Text("$100+")
+                    }
+                    .padding(.horizontal)
+                    
+                    Text("$\(Int(budget))")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
                 }
-                .frame(height: 40)
-
-                HStack {
-                    Text("$5")
-                    Spacer()
-                    Text("$100+")
+                
+                HStack(spacing: 40) {
+                    Button("Back") { //navigation between during interfaces
+                        dismiss()
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    NavigationLink(destination: NavigationPage()){
+                        Button("Next") {
+                            print("Selected budget: \(Int(budget))")
+                        }
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    }
                 }
-                .padding(.horizontal)
-
-                Text("$\(Int(budget))")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
-
-            HStack(spacing: 40) {
-                Button("Back") { //navigation between during interfaces
-                    dismiss()
-                }
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.gray)
-                .cornerRadius(10)
-
-                Button("Next") {
-                    print("Selected budget: \(Int(budget))")
-                }
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
             }
         }
         .padding()
