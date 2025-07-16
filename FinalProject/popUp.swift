6478f370f4a837c996a4bc425f649f74c93943e8
Medha Manda <medhamanda9@gmail.com>
2025-07-16 11:52:03 -0400

    created pop-up screen

---
+++ FinalProject/popUp.swift
@@ -0,0 +1,113 @@
+//
+//  popUp.swift
+//  FinalProject
+//
+//  Created by Medha Manda on 7/15/25.
+//
+
+import SwiftUI
+
+struct popUp: View {
+    var body: some View {
+        ZStack{
+            Color(red: 0.97, green: 0.96, blue: 0.922)
+                .ignoresSafeArea()
+            VStack{
+                HStack {
+                    Text("Pop-Ups")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.836, green: 0.332, blue: 0.313))
+                        .padding()
+                    Spacer()
+                }
+                TextField("Search here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius:9).stroke(Color.gray, lineWidth: 1))
+                    .padding(.all)
+                VStack {
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 9).fill(Color(red: 0.217, green: 0.337, blue: 0.528))
+                                .padding(.all)
+                                .frame(height: 200.0)
+                            VStack{
+                                Text("Panda Express' Orange Chicken Kitchen Pop-Up")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                                Text("July 15 - July 20")
+                                    .multilineTextAlignment(.leading)
+                                    .padding(.leading)
+                                    .foregroundColor(.white)
+                                Text("📍835 3rd Ave, New York, NY")
+                                    .font(.callout)
+                                    .foregroundColor(.white)
+                                
+                            }
+                        }
+                    }
+                }
+                Spacer()
+                    .frame(height: 5)
+                VStack {
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 9).fill(Color(red: 0.836, green: 0.332, blue: 0.313))
+                                .padding(.all)
+                                .frame(height: 200.0)
+                            VStack{
+                                Text("Laneige x Baskin Robbins National    Ice Cream Day")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                                Text("July 15 - August 4")
+                                    .multilineTextAlignment(.leading)
+                                    .padding(.leading)
+                                    .foregroundColor(.white)
+                                Text("📍Times Square 45-46 Plaza                                1535 Broadway, New York, NY")
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                                    .foregroundColor(.white)
+                                
+                            }
+                        }
+                    }
+                }
+                Spacer()
+                    .frame(height: 5)
+                VStack {
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 9).fill(Color(red: 0.217, green: 0.337, blue: 0.528))
+                                .padding(.all)
+                                .frame(height: 200.0)
+                            VStack{
+                                Text("Crystal Hot Sauce x Morgenstern's         Ice Cream Collab")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                                Text("July 19 - July 20")
+                                    .multilineTextAlignment(.leading)
+                                    .padding(.leading)
+                                    .foregroundColor(.white)
+                                Text("📍88 West Houston Street, New York, NY")
+                                    .foregroundColor(.white)
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+    
+    #Preview {
+        popUp()
+    }
